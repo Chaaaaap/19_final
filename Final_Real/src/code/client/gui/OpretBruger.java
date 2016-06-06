6adc1cc8d3dfe4db39de0b3f47afc4aad8c0b34c
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-06 21:17:17 +0200

    #nemt

--- Final_Real/src/code/client/gui/OpretBruger.java
+++ Final_Real/src/code/client/gui/OpretBruger.java
@@ -2,7 +2,11 @@ package code.client.gui;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class OpretBruger extends Composite {
@@ -15,5 +19,13 @@ public class OpretBruger extends Composite {
 	public OpretBruger() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
+	
+	@UiField TextBox boxID;
+	@UiField TextBox boxNavn;
+	@UiField TextBox boxIni;
+	@UiField TextBox boxCPR;
+	@UiField TextBox boxPassword;
+	@UiField ListBox boxType;
+	@UiField Button submit;
 
 }
