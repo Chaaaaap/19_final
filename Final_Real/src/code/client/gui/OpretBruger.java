7470524bb88d2ef67d1a5ca8f0d5f9860fe47e38
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-07 10:17:22 +0200

    Suuuuuup

--- Final_Real/src/code/client/gui/OpretBruger.java
+++ Final_Real/src/code/client/gui/OpretBruger.java
@@ -1,11 +1,15 @@
 package code.client.gui;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -24,8 +28,21 @@ public class OpretBruger extends Composite {
 	@UiField TextBox boxNavn;
 	@UiField TextBox boxIni;
 	@UiField TextBox boxCPR;
-	@UiField TextBox boxPassword;
+	@UiField PasswordTextBox boxPassword;
+	@UiField PasswordTextBox boxGentag;
 	@UiField ListBox boxType;
+	@UiField Label labelError;
 	@UiField Button submit;
+	
+//	@UiHandler("boxGentag")
+//	void
+	
+	@UiHandler("submit")
+	void opretBruger(ClickEvent e) {
+		if(boxID.getText() == "" || boxNavn.getText() == "" || boxCPR.getText() == "" || 
+				boxPassword.getText() == "" || boxType.getSelectedItemText() == "") {
+			
+		}
+	}
 
 }
