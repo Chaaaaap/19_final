8013750cfad37a899f930fc66f6fd62234651177
IamHexor <ykhexor@hotmail.com>
2016-06-07 11:56:24 +0200

    Halla halla

--- Final_Real/src/code/client/gui/OpretRaavarebatch.java
+++ Final_Real/src/code/client/gui/OpretRaavarebatch.java
@@ -2,7 +2,10 @@ package code.client.gui;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class OpretRaavarebatch extends Composite {
@@ -16,4 +19,9 @@ public class OpretRaavarebatch extends Composite {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
+	@UiField TextBox boxID;
+	@UiField TextBox boxRaavareNr;
+	@UiField TextBox boxMaengde;
+	@UiField Button submit;
+	
 }
