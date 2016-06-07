291d2d30ccffc973f215864682c7c646c0ed0f14
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-07 14:16:32 +0200

    RaavareDAO har lidt metoder nu (uden bodies)

--- Final_Real/src/code/server/IRaavareDAO.java
+++ Final_Real/src/code/server/IRaavareDAO.java
@@ -1,5 +1,10 @@
 package code.server;
 
+import java.util.ArrayList;
+
 public interface IRaavareDAO {
 
+	void addRaavare();
+	ArrayList<String> getRaavarer();
+	
 }
--- Final_Real/src/code/server/RaavareDAO.java
+++ Final_Real/src/code/server/RaavareDAO.java
@@ -1,5 +1,20 @@
 package code.server;
 
+import java.util.ArrayList;
+
 public class RaavareDAO implements IRaavareDAO {
 
+	@Override
+	public void addRaavare() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<String> getRaavarer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
