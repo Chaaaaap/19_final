efbf82d2ab9b951d7e363bba17f6408cc81d8810
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-08 11:59:19 +0200

    Alle services oprettet

---
+++ Final_Real/src/code/server/RaavareBatchServiceImpl.java
@@ -0,0 +1,10 @@
+package code.server;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+import code.client.service.IRaavareBatchService;
+
+@SuppressWarnings("serial")
+public class RaavareBatchServiceImpl extends RemoteServiceServlet implements IRaavareBatchService {
+
+}
