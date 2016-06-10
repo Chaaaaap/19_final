7b3ab236dda2fedd5200fa9818655db6e276b2f6
Martin <Martin_rodgaard@hotmail.dk>
2016-06-10 09:06:40 +0200

    lol

--- Final_Real/src/code/client/gui/Afvejning.java
+++ Final_Real/src/code/client/gui/Afvejning.java
@@ -7,28 +7,36 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 import code.client.service.VsConClient;
+import code.shared.OperatoerDTO;
 
 public class Afvejning extends Composite {
 
+	private OperatoerDTO dto;
 	private static AfvejningUiBinder uiBinder = GWT.create(AfvejningUiBinder.class);
 	private VsConClient vsclient;
 	
 	interface AfvejningUiBinder extends UiBinder<Widget, Afvejning> {
 	}
 
-	public Afvejning() {
+	public Afvejning(OperatoerDTO oprDTO) {
 		initWidget(uiBinder.createAndBindUi(this));
 		this.vsclient = new VsConClient(GWT.getHostPageBaseURL()+"19_Final_afvejning");
+		this.dto = oprDTO;
 	}
 
 	@UiField
-	Button afvej;
+	Label label;
+	TextBox textbox;
+	Button OK;
 
-	@UiHandler("afvej")
+	@UiHandler("OK")
 	void onClick(ClickEvent e) {
+		if(Integer.parseInt(textbox.getText()) == dto.getOprID())
 		vsclient.Simulator();
 	}
 	
