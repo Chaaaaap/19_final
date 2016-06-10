89ff3d24e71b3f7d710ee7e189d2aa0751a45bcc
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-10 17:40:43 +0200

    dto

---
+++ Final_Real/src/code/shared/ProduktBatchKomponentDTO.java
@@ -0,0 +1,11 @@
+package code.shared;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class ProduktBatchKomponentDTO implements Serializable {
+	
+	public ProduktBatchKomponentDTO() {}
+		
+	
+}
