96a90e26ddeb69ae9058ab8acdf2176fe1a59ade
Silas El-Azm Stryhn <s143599@student.dtu.dk>
2016-06-13 10:49:53 +0200

    produktbatchkomponent DTO

--- Final_Real/src/code/shared/ProduktBatchKomponentDTO.java
+++ Final_Real/src/code/shared/ProduktBatchKomponentDTO.java
@@ -6,6 +6,36 @@ import java.io.Serializable;
 public class ProduktBatchKomponentDTO implements Serializable {
 	
 	public ProduktBatchKomponentDTO() {}
+	
+	private int pb_id, rb_id, tara, netto, oprID;
+	
+	public ProduktBatchKomponentDTO(int pb_id, int rb_id, int tara, int netto, int oprID) {
+	this.pb_id = pb_id;
+	this.rb_id = rb_id;
+	this.tara = tara;
+	this.netto = netto;
+	this.oprID = oprID;
+	}
+
+	public int getPb_id() {
+		return pb_id;
+	}
+
+	public int getRb_id() {
+		return rb_id;
+	}
+
+	public int getTara() {
+		return tara;
+	}
+
+	public int getNetto() {
+		return netto;
+	}
+	
+	public int getOprID() {
+		return oprID;
+	}
 		
 	
 }
