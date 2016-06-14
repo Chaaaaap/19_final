2cd38f008810a9b3a3d1dedeec5ee1229bc3d2d3
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-14 16:50:09 +0200

    Frank er Bambi

---
+++ Final_Real/src/code/shared/validate/Validator.java
@@ -0,0 +1,63 @@
+package code.shared.validate;
+
+import java.io.Serializable;
+
+import code.shared.DALException;
+
+@SuppressWarnings("serial")
+public class Validator implements Serializable {
+
+
+	public Validator() {
+	}
+
+	public boolean validateInt(String value) {
+		try {
+			Integer.parseInt(value);
+		} catch(NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean validateDouble(String value) {
+		try {
+			Double.parseDouble(value);
+		} catch(NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean validatePassword(String password) {
+		try {
+			if(password.length() < 6) {
+				throw new DALException("Passwordet er under 6 karakterer");
+			}
+			char x;
+			int smallLetters = 0;
+			int capLetters = 0;
+			int numbers = 0;
+			for (int i = 0; i < password.length(); i++) {
+				x = password.charAt(i);
+				if(x < 'Z' && x > 'A') {
+					capLetters = 1;
+				} else if(x < 'z' && x > 'a') {
+					smallLetters = 1;
+				} else if(x < '9' && x > '0') {
+					numbers = 1;
+				}
+			}
+			if((capLetters + smallLetters + numbers) < 3) {
+				throw new DALException("Passwordet er ikke godt nok");
+			}
+			
+			return true;
+			
+		} catch(DALException e) {
+			return false;
+		}
+		
+	}
+
+}
