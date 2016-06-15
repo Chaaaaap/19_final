eed6cd3d5bc4068351840f617dd6bdf37b2a3c0f
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-15 15:44:42 +0200

    Påbegyndt næste test

--- Final_Real/src/code/test/OperatoerDAOTest.java
+++ Final_Real/src/code/test/OperatoerDAOTest.java
@@ -1,14 +1,34 @@
 package code.test;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
+import code.server.OperatoerDAO;
+import code.shared.DALException;
+import code.shared.OperatoerDTO;
+
 public class OperatoerDAOTest {
+	
+	OperatoerDAO oprDAO = new OperatoerDAO();
+	OperatoerDTO opr;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testOpretOperatoerPositiv() {
+		int x = 100;
+		try {
+		opr = new OperatoerDTO(x, "Smølf", "S", "1234589685", "Hej123", 1, "administrator");
+		oprDAO.opretBruger(x, "Smølf", "S", "1234589685", "Hej123", "administrator");
+		
+		Assert.assertEquals(oprDAO.getOperatoer(x), opr);
+		} catch(DALException e) {
+			
+		} 
+		
+	}
+	
+	@Test
+	public void testRedigerOperatoerPositiv() {
+		
 	}
 
 }
