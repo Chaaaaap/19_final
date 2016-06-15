85872105960e1d20b12f5a3f33338940592611ae
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-15 21:32:43 +0200

    RaavareDAOTest done (med et dirty workaround)

--- Final_Real/src/code/test/RaavareDAOTest.java
+++ Final_Real/src/code/test/RaavareDAOTest.java
@@ -1,14 +1,50 @@
 package code.test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import code.server.RaavareDAO;
+import code.shared.DALException;
+import code.shared.RaavareDTO;
 
 public class RaavareDAOTest {
 
+	RaavareDAO rDAO = new RaavareDAO();
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetReceptList() {
+		try {
+			ArrayList<RaavareDTO> rList = rDAO.getRaavarer();
+			Assert.assertNotNull(rList);
+		} catch(DALException e) {
+
+		}
 	}
 
+	@Test
+	public void testOpretRaavare() {
+		try {
+			rDAO.addRaavare(666, "Test", "TestLand");
+			ArrayList<RaavareDTO> rList = rDAO.getRaavarer();
+			Assert.assertEquals("Test", rList.get(rList.size()-1).getRaavare_navn());
+		} catch(DALException e) {
+
+		}
+	}
+
+	@Test
+	public void testRedigerRaavare() {
+		int nytID = 667;
+		int glID = 666;
+		try {
+			rDAO.redigerRaavare(nytID, "Test", "TestLand", glID);
+			ArrayList<RaavareDTO> rList = rDAO.getRaavarer();
+			Assert.assertEquals("Test", rList.get(rList.size()-1).getRaavare_navn());
+		} catch(DALException e) {
+
+		}
+	}
+	
 }
