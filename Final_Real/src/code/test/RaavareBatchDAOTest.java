19f2f55780c9ddc7c64cd48b1847e9145b09343e
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-15 22:04:24 +0200

    RaavareBatchDTOTest dåååååne

--- Final_Real/src/code/test/RaavareBatchDAOTest.java
+++ Final_Real/src/code/test/RaavareBatchDAOTest.java
@@ -1,14 +1,51 @@
 package code.test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import code.server.RaavareBatchDAO;
+import code.shared.DALException;
+import code.shared.RaavareBatchDTO;
 
 public class RaavareBatchDAOTest {
 
+	RaavareBatchDAO rbDAO = new RaavareBatchDAO();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetRaavarebatchList() {
+		try {
+			ArrayList<RaavareBatchDTO> rbList = rbDAO.getRaavareBatch();
+			Assert.assertNotNull(rbList);
+		} catch(DALException e) {
+			
+		}
+	}
+	
+	@Test
+	public void testOpretRaavarebatch() {
+		int id = 666;
+		try {
+			rbDAO.addRaavareBatch(id, 1, 50);
+			ArrayList<RaavareBatchDTO> rbList = rbDAO.getRaavareBatch();
+			Assert.assertEquals(id, rbList.get(rbList.size()-1).getRaavareBatch_id());
+		} catch(DALException e) {
+			
+		}
+	}
+	
+	@Test
+	public void testRedigerRaavarebatch() {
+		int nyID = 667;
+		int glID = 666;
+		try {
+			rbDAO.redigerRaavareBatch(nyID, 1, 50, glID);
+			ArrayList<RaavareBatchDTO> rbList = rbDAO.getRaavareBatch();
+			Assert.assertEquals(nyID, rbList.get(rbList.size()-1).getRaavareBatch_id());
+		} catch(DALException e) {
+			
+		}
 	}
 
 }
