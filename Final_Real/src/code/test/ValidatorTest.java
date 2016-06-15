461787e9f6b5c4383fa17f1fe79f2972461b5420
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-15 15:05:57 +0200

    validate test done!

--- Final_Real/src/code/test/ValidatorTest.java
+++ Final_Real/src/code/test/ValidatorTest.java
@@ -23,6 +23,92 @@ public class ValidatorTest {
 		Assert.assertTrue(test);
 	}
 	
+	@Test
+	public void testValidateDoublePositive() {
+		boolean test = val.validateDouble("100.0");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidateDoubleNegative() {
+		boolean test = val.validateDate("Søren");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidatePasswordPositive1() {
+		boolean test = val.validatePassword("Hej123");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidatePasswordNegative() {
+		boolean test = val.validatePassword("MusenErGrimtoOgtyve");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidatePasswordNegative2() {
+		boolean test = val.validatePassword("Muse2");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidatePasswordNegative3() {
+		boolean test = val.validatePassword("test1234");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidateDatePositive1() {
+		boolean test = val.validateDate("2016-07-19");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidateDatePositive2() {
+		boolean test = val.validateDate("2016-02-29");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidateDateNegative1() {
+		boolean test = val.validateDate("2001-02-29");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidateDateNegative2() {
+		boolean test = val.validateDate("2016-00-01");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidateDateNegative3() {
+		boolean test = val.validateDate("2016-01-00");
+		
+		Assert.assertTrue(test);
+	}
+	
+	@Test
+	public void testValidateDateNegative4() {
+		boolean test = val.validateDate("2016-09-31");
+		
+		Assert.assertTrue(test);
+	}
+	
+	
+	
 	
 
 }
