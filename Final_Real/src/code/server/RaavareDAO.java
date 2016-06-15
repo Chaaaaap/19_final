6fcc8dda754a4863962034bf70c77601b201b9bc
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-15 21:22:03 +0200

    Ændret exception typerne

--- Final_Real/src/code/server/RaavareDAO.java
+++ Final_Real/src/code/server/RaavareDAO.java
@@ -1,9 +1,11 @@
 package code.server;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import code.connector.Connector;
+import code.shared.DALException;
 import code.shared.RaavareDTO;
 
 public class RaavareDAO implements IRaavareDAO {
@@ -11,55 +13,55 @@ public class RaavareDAO implements IRaavareDAO {
 	private Connector connector = new Connector();
 
 	@Override
-	public void addRaavare(int raavare_id, String raavare_navn, String leverandør) throws Exception{
+	public void addRaavare(int raavare_id, String raavare_navn, String leverandør) throws DALException{
 		String query = "INSERT INTO raavare VALUES("+raavare_id+
 					", '"+raavare_navn+"', '"+leverandør+"');";
 		try {
 			connector.doUpdate(query);
-		} catch(Exception e) {
-			throw e;
+		} catch(SQLException e) {
+			throw new DALException(e.getMessage());
 		}
 	}
 
 	@Override
-	public ArrayList<RaavareDTO> getRaavarer() throws Exception {
+	public ArrayList<RaavareDTO> getRaavarer() throws DALException {
 		ArrayList<RaavareDTO> rvList = new ArrayList<RaavareDTO>();
 		ResultSet rs;
 		try {
 			rs = connector.doQuery("SELECT * FROM raavare");
-			if(!rs.next()) throw new Exception("Listen er tom");
+			if(!rs.next()) throw new DALException("Listen er tom");
 			do {
 				rvList.add(new RaavareDTO(rs.getInt("raavare_id"), rs.getString("raavare_navn"),
 						rs.getString("leverandoer")));
 			} while(rs.next());
-		} catch(Exception e) {
-			throw e;
+		} catch(SQLException e) {
+			throw new DALException(e.getMessage());
 		} 
 		return rvList;
 		
 	}
 
 	@Override
-	public void redigerRaavare(int raavare_id, String raavare_navn, String leverandør, int glid) throws Exception {
+	public void redigerRaavare(int raavare_id, String raavare_navn, String leverandør, int glid) throws DALException {
 		try {
 		connector.doUpdate("UPDATE raavare SET raavare_id = "+raavare_id+", raavare_navn = '"
 		+raavare_navn+"', leverandoer = '"+leverandør+"' WHERE raavare_id = "+glid+";");		
-		} catch(Exception e) {
-			throw e;
+		} catch(SQLException e) {
+			throw new DALException(e.getMessage());
 		}
 	}
 	
 	@Override
-	public String getRaavare(int pb_id) throws Exception {
+	public String getRaavare(int pb_id) throws DALException {
 		ResultSet rs;
 		String navn;
 		try {
 			rs = connector.doQuery("SELECT raavare_navn FROM produktbatch natural join produktbatchkomponent natural join raavarebatch natural join raavare where pb_id ="+pb_id +" group by raavare_navn;");
 			navn = rs.getString("raavare_navn");
-			if(!rs.next()) throw new Exception("Raavaren findes ikke");
+			if(!rs.next()) throw new DALException("Raavaren findes ikke");
 			
-		} catch(Exception e) {
-			throw e;
+		} catch(SQLException e) {
+			throw new DALException(e.getMessage());
 		} 
 		return navn;
 		
