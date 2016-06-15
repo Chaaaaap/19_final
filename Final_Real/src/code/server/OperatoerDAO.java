d28b2abf5c89f35a64d6be5faca6bb06acb17b80
Mikkel Hansen <mikkelhansen.92@jubii.dk>
2016-06-15 15:29:46 +0200

    Ændret exception typer

--- Final_Real/src/code/server/OperatoerDAO.java
+++ Final_Real/src/code/server/OperatoerDAO.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import code.connector.Connector;
+import code.shared.DALException;
 import code.shared.OperatoerDTO;
 
 public class OperatoerDAO implements IOperatoerDAO {
@@ -16,54 +17,54 @@ public class OperatoerDAO implements IOperatoerDAO {
 	}
 	
 	@Override
-	public OperatoerDTO getOperatoer(int oprID) throws Exception{
+	public OperatoerDTO getOperatoer(int oprID) throws DALException{
 		
 	    try {
 	    	ResultSet rs = connector.doQuery("SELECT * FROM operatoer WHERE opr_id = '" + oprID+"'");
-	    	if (!rs.first()) throw new Exception("Operatoeren '" + oprID + "' findes ikke");
+	    	if (!rs.first()) throw new DALException("Operatoeren '" + oprID + "' findes ikke");
 	    	return new OperatoerDTO (rs.getInt("opr_id"), rs.getString("opr_navn"), rs.getString("ini"), 
 	    			rs.getString("cpr"), rs.getString("password"), Integer.parseInt(rs.getString("aktiv")), rs.getString("type"));
 	    }
 	    catch (SQLException e) {
-	    	throw e;
+	    	throw new DALException(e.getMessage());
 	    }
 	}
 	
 	@Override
-	public void skiftPassword(int oprID, String nyPassword) throws Exception {
+	public void skiftPassword(int oprID, String nyPassword) throws DALException {
 		try {
 			connector.doUpdate("UPDATE operatoer SET password = '"+nyPassword+"' WHERE opr_id = '"+oprID+"'");
 		} catch(SQLException e) {
-			throw e;
+			throw new DALException(e.getMessage());
 		}
 	}
 
 	@Override
 	public void opretBruger(int oprID, String navn, String ini, String CPR, String password, String type)
-			throws Exception {
+			throws DALException {
 		try {
 			connector.doUpdate("INSERT INTO operatoer(opr_id, opr_navn, ini, cpr, password, aktiv, type)"
 					+ " VALUES " + "(" + oprID + ", '" + navn + "', '" + ini + "', '" + 
 					CPR + "', '" + password + "'"+", '1', '"+type+"')");
 			
 		} catch(SQLException e) {
-			throw e;
+			throw new DALException(e.getMessage());
 		}
 	}
 
 	@Override
-	public ArrayList<OperatoerDTO> getOperatoerer() throws Exception {
+	public ArrayList<OperatoerDTO> getOperatoerer() throws DALException {
 		ArrayList<OperatoerDTO> oprList = new ArrayList<OperatoerDTO>();
 		ResultSet rs;
 		try {
 			rs = connector.doQuery("SELECT * FROM grp19.operatoer");
-			if(!rs.next()) throw new Exception("Listen er tom");
+			if(!rs.next()) throw new DALException("Listen er tom");
 			do {
 				oprList.add(new OperatoerDTO(rs.getInt("opr_id"), rs.getString("opr_navn"),
 						rs.getString("ini"), rs.getString("cpr"), rs.getString("password"), rs.getInt("aktiv"), rs.getString("type")));
 			} while(rs.next());
-		} catch(Exception e) {
-			throw e;
+		} catch(SQLException e) {
+			throw new DALException(e.getMessage());
 		} 
 		return oprList;
 	}
@@ -78,20 +79,20 @@ public class OperatoerDAO implements IOperatoerDAO {
 		}
 	}
 
-	public void deaktiverBruger(int oprID, int aktiv) throws Exception{
+	public void deaktiverBruger(int oprID, int aktiv) throws DALException{
 		try {
 			connector.doUpdate("UPDATE operatoer SET aktiv = "+0+" WHERE opr_id = "+oprID+" ;");		
-			} catch(Exception e) {
-				throw e;
+			} catch(SQLException e) {
+				throw new DALException(e.getMessage());
 			}
 		
 	}
 	
-	public void aktiverBruger(int oprID, int aktiv) throws Exception{
+	public void aktiverBruger(int oprID, int aktiv) throws DALException{
 		try {
 			connector.doUpdate("UPDATE operatoer SET aktiv = "+1+" WHERE opr_id = "+oprID+" ;");		
-			} catch(Exception e) {
-				throw e;
+			} catch(SQLException e) {
+				throw new DALException(e.getMessage());
 			}
 		
 	}
